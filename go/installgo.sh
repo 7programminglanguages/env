33e8c5273e49b90c22089123ee0af8049b973658
732047249 <732047249@qq.com>
2019-12-21 15:21:13 +0800

    tested

--- go/installgo.sh
+++ go/installgo.sh
@@ -1,3 +1,3 @@
-wget https://studygolang.com/dl/golang/go1.12.5.linux-amd64.tar.gz
-tar -zxvf go1.12.5.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz
+tar -zxvf go1.10.1.linux-amd64.tar.gz
 sudo mv go /usr/local/
