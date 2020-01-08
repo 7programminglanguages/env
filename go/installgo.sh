d893be1b93abca8f27952ca8c8749a87f8ea8f35
732047249 <732047249@qq.com>
2020-01-08 22:07:49 +0800

    update go version to latest

--- go/installgo.sh
+++ go/installgo.sh
@@ -1,5 +1,6 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install wget
-wget https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz
+#wget https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
 tar -zxvf go1.10.1.linux-amd64.tar.gz
 sudo mv go /usr/local/
